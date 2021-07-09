8b8d882240dd40fd4b7774c7289f1ab3493575ec
Florian <florian.martin63000@gmail.com>
2021-07-10 01:53:51 +0200

    Fix: some problems with Cacheable annotation

--- src/main/java/com/florian935/rediscache/config/CacheConfig.java
+++ src/main/java/com/florian935/rediscache/config/CacheConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableCaching
@@ -24,10 +26,16 @@ public class CacheConfig {
     }
 
     @Bean
-    RedisTemplate<String, ?> redisTemplate() {
+    RedisTemplate<String, Object> redisTemplate() {
 
-        final RedisTemplate<String, ?> redisTemplate = new RedisTemplate<>();
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new JdkSerializationRedisSerializer());
+        redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
+        redisTemplate.setEnableTransactionSupport(true);
+        redisTemplate.afterPropertiesSet();
 
         return redisTemplate;
     }
